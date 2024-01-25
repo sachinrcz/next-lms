2e88d328c2668c902b6bf6cf66aeac28976f1dbb
Sachin <sachin_rcz@yahoo.co.in>
2024-01-25 17:28:34 +0800

    course creation UI

---
+++ app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils"
+import Image from "next/image"
+import Link from "next/link"
+
+export const Logo = () => {
+    return (
+        <Link href="/">
+            <div className="hover:opacity-75 transition items-center gap-x-2 flex">
+            <Image
+                src="/logo.svg"
+                alt="Logo"
+                height={30}
+                width={30}
+            />
+            <p className={cn("text-lg text-neutral-700 pb-1")}>
+                Devicion LMS
+            </p>
+            </div>
+        </Link>
+    )
+}
