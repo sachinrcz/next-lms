714f4f7295cad884fca823226604e9e90aacc9ee
Sachin <sachin_rcz@yahoo.co.in>
2024-02-04 17:22:20 +0800

    updates

--- app/(dashboard)/(routes)/home/page.tsx
+++ app/(dashboard)/(routes)/home/page.tsx
@@ -1,11 +1,10 @@
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
-import Link from "next/link";
 
 export default function Home(){
   return (
     <div>
-      <Link href="/sign-in">Log in</Link>
+      <UserButton afterSwitchSessionUrl="/"/>
     </div>
     
   )
