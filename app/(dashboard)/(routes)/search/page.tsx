ceef96354335ab8601aeab72c881b7c42cf4cd1b
Sachin <sachin_rcz@yahoo.co.in>
2024-03-28 21:03:43 +0800

    search categories

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,8 +1,28 @@
-const SearchPage = () => {
+import { db } from "@/lib/db";
+import { Categories } from "./_components/categories";
+import { SearchInput } from "@/components/search-input";
+
+const SearchPage = async() => {
+
+    const categories = await db.category.findMany({
+        orderBy: {
+            name: "asc",
+        }
+    });
+
+
     return ( 
-        <div>
-            this is a search page
-        </div> 
+        <>
+            <div className="px-6 pt-6 md:hidden md:mb-0-block">
+                <SearchInput />
+
+            </div>
+            <div className="p-6">
+                <Categories
+                    items={categories}
+                />
+            </div> 
+        </>
     );
 }
  
