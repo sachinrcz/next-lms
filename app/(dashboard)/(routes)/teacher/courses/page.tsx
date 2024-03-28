a752b790d46ba0d8820ad27be23e65e8ce88d97c
Sachin <sachin_rcz@yahoo.co.in>
2024-03-28 19:58:07 +0800

    course data table

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,15 +1,30 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
-const CoursePage = () => {
+  
+const CoursePage = async() => {
+    const {userId} = auth();
+    if (!userId){
+        return redirect("/");
+    }
+    const course = await db.course.findMany({
+        where:{
+            userId,
+        }, 
+        orderBy: {
+            createdAt: "desc",
+        }
+    })
     return ( 
         <div className="p-6">
-            <Link href="/teacher/create">
-                <Button>
-                    New Course
-                </Button>
-            
-            </Link>
+             <div className="container mx-auto py-10">
+                <DataTable columns={columns} data={course} />
+            </div>
             
         </div> 
     );
