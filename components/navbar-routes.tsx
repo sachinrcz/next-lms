e4af49e1dcb619f6e351370e2ad493baf0350261
Sachin <sachin_rcz@yahoo.co.in>
2024-04-09 15:03:44 +0800

    admin protection

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs"
+import { useAuth, UserButton } from "@clerk/nextjs"
 import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
 import { SearchInput } from "./search-input";
+import { isTeacher } from "@/lib/teacher";
 
 export const NavbarRoutes = () => {
+
+    const {userId} = useAuth();
 
     const pathname = usePathname();
 
@@ -31,13 +34,13 @@ export const NavbarRoutes = () => {
                     </Button>
                     </Link>
                     
-                ): (
+                ): isTeacher(userId) ? (
                     <Link href="/teacher/courses">
                         <Button size="sm" variant="ghost">
                             Teacher mode
                         </Button>
                     </Link>
-                )}
+                ) : null}
                 <UserButton 
                     afterSignOutUrl="/"
                 />
